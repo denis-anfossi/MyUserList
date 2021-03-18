57f362d6e68d4117d9b2de921831bd4e346e99c8
Denis Anfossi <denis.anfossi.job@gmail.com>
2021-03-18 12:32:04 +0100

    V3 Create User completed

--- app/src/main/java/com/denisanfossi/myuserlist/ui/CreateUserActivity.java
+++ app/src/main/java/com/denisanfossi/myuserlist/ui/CreateUserActivity.java
@@ -1,20 +1,39 @@
 package com.denisanfossi.myuserlist.ui;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.denisanfossi.myuserlist.data.repository.UsersRepository;
 import com.denisanfossi.myuserlist.databinding.ActivityCreateUserBinding;
+import com.denisanfossi.myuserlist.di.Injection;
+import com.denisanfossi.myuserlist.model.User;
 
 public class CreateUserActivity extends AppCompatActivity {
 
     private ActivityCreateUserBinding mBinding;
+    private UsersRepository mUsersRepository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = ActivityCreateUserBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
+        mUsersRepository = Injection.getUsersRepository();
+        mBinding.addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = mBinding.nameEdittext.getText().toString();
+                if (!name.isEmpty()) {
+                    Long id = mUsersRepository.getUsers().get(mUsersRepository.getUsers().size() - 1).getId() + 1;
+                    String avatarUrl = "https://avatars.dicebear.com/api/bottts/" + name + ".png";
+                    User user = new User(id, name, avatarUrl);
+                    mUsersRepository.createUser(user);
+                    finish();
+                }
+            }
+        });
     }
 
 }
